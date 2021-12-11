00ad5dc38fe6e309c8fde45b59a8f2da93268046
Shanta Maria <shantamaria@iut-dhaka.edu>
2021-12-11 17:59:27 +0600

    Lab 03 (03.12.21)

---
+++ Lab 03 CarRentalSystem/CRS_ClassLibrary/User.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRS_ClassLibrary
+{
+    public class User
+    {
+        private int user_id;
+        private string user_name;
+        private string address;
+        private string destination;
+        private string rented_car;
+
+        public User(int user_id, string user_name, string address, string destination)
+        {
+            this.user_id = user_id;
+            this.user_name = user_name;
+            this.address = address;
+            this.destination = destination;
+        }
+
+        public int getUserID
+        {
+            get { return user_id; }
+        }
+
+        public string getUserName
+        {
+            get { return user_name; }
+        }
+
+        public string getAddress
+        {
+            get { return address; }
+        }
+
+        public string getDestination
+        {
+            get { return destination; }
+        }
+
+        public string getRentedCar
+        {
+            get { return rented_car; }
+        }
+
+        public string setRentedCar
+        {
+            set { rented_car = value; }
+        }
+    }
+}
